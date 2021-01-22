babcd9d3dc576e3216b31ac272a89a92db2b3f29
evandrogouveia <vando_gouveia@hotmail.com>
2021-01-22 18:26:32 -0300

    add project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule) },
+  {
+    path: 'private',
+    loadChildren: () => import('./modules/private/private.module').then(m => m.PrivateModule),
+
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrivateComponent } from './modules/private/private.component';
+import { PublicComponent } from './modules/public/public.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrivateComponent,
+    PublicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
