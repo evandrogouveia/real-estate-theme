08dc2ab4c9546c747479bde3aac0b35c96ab5422
evandrogouveia <vando_gouveia@hotmail.com>
2021-01-23 14:58:16 -0300

    carousel

--- src/app/modules/public/pages/home/home.component.ts
+++ src/app/modules/public/pages/home/home.component.ts
@@ -18,8 +18,9 @@ export class HomeComponent implements OnInit {
     autoplay: true,
     center: true,
     dots: false,
-    autoHeight: true,
+    autoHeight: false,
     autoWidth: true,
+    navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
     responsive: {
       0: {
         items: 1
