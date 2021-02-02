3f64e407de1e4d79ef76ac2c96b84bffd62b7705
evandrogouveia <vando_gouveia@hotmail.com>
2021-02-02 16:09:33 -0300

    add module admin

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PrivateRoutingModule } from './private-routing.module';
+import { AdminComponent } from './admin/admin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminComponent],
   imports: [
     CommonModule,
     PrivateRoutingModule
