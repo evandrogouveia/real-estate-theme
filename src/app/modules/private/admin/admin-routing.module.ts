5b391475a8ff37611f1fd9bf0fb9baf26a53be39
evandrogouveia <vando_gouveia@hotmail.com>
2021-02-03 18:11:15 -0300

    admin posts

--- src/app/modules/private/admin/admin-routing.module.ts
+++ src/app/modules/private/admin/admin-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
+import { AddPostsComponent } from './components/add-posts/add-posts.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     component: AdminComponent,
     children: [
       {path: 'home', component: HomeComponent },
+      {path: 'add-posts', component: AddPostsComponent},
       {path: '', pathMatch: 'full', redirectTo: 'home'},
     ],
   },
--- src/app/modules/private/admin/admin.module.ts
+++ src/app/modules/private/admin/admin.module.ts
@@ -8,10 +8,11 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NgxBootstrapModule } from 'src/app/ngx-bootstrap.module';
+import { AddPostsComponent } from './components/add-posts/add-posts.component';
 
 
 @NgModule({
-  declarations: [AdminComponent, HomeComponent, HeaderComponent, SidebarComponent],
+  declarations: [AdminComponent, HomeComponent, HeaderComponent, SidebarComponent, AddPostsComponent],
   imports: [
     CommonModule,
     AdminRoutingModule,
