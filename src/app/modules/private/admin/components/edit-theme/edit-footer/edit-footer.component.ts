e5ffab843692ef60b5fc10b47a637fafeb5900e0
evandrogouveia435 <vando_gouveia@hotmail.com>
2021-02-06 13:24:51 -0300

    edit footer

--- src/app/modules/private/admin/components/edit-theme/edit-footer/edit-footer.component.ts
+++ src/app/modules/private/admin/components/edit-theme/edit-footer/edit-footer.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-footer.component.scss']
 })
 export class EditFooterComponent implements OnInit {
-
+  imagemSrc = 'assets/img/placeholder.jpg';
+  selectedImage: any = null;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showPreviewImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imagemSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    } else {
+      this.imagemSrc = 'assets/img/icons/user-empty.svg';
+      this.selectedImage = null;
+    }
   }
 
 }
