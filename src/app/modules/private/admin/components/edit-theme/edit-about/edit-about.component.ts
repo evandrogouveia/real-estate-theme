6cbd9fd4be595b45016fc4378715ccfcdd9a2b1f
evandrogouveia <vando_gouveia@hotmail.com>
2021-02-11 14:20:25 -0300

    edit about finalize

--- src/app/modules/private/admin/components/edit-theme/edit-about/edit-about.component.ts
+++ src/app/modules/private/admin/components/edit-theme/edit-about/edit-about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-edit-about',
@@ -6,10 +8,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-about.component.scss']
 })
 export class EditAboutComponent implements OnInit {
+  imagemSrc = 'assets/img/placeholder.jpg';
+  selectedImage: any = null;
+  checked: boolean = true;
 
-  constructor() { }
+  aboutFooterForm: FormGroup = this.fb.group({
+    htmlContentDescription1: [''],
+    htmlContentDescription2: [''],
+    htmlContentDescription3: [''],
+  });
+
+  constructor(private fb: FormBuilder,) { }
 
   ngOnInit(): void {
   }
+
+  showPreviewImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => this.imagemSrc = e.target.result;
+      reader.readAsDataURL(event.target.files[0]);
+      this.selectedImage = event.target.files[0];
+    } else {
+      this.imagemSrc = 'assets/img/icons/user-empty.svg';
+      this.selectedImage = null;
+    }
+  }
+
+  isChecked(event){
+    this.checked = event.target.checked;
+  }
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '10rem',
+    minHeight: '1rem',
+    placeholder: 'Digite o texto aqui...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    toolbarHiddenButtons: [
+      [
+        'insertImage',
+        'insertVideo',
+        'customClasses',
+        'insertUnorderedList',
+        'insertOrderedList',
+      ]
+      ],
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ],
+    
+  };
 
 }
