38af94ffb382004ca0749fd996ed6191ca385947
evandrogouveia435 <vando_gouveia@hotmail.com>
2021-02-18 23:49:45 -0300

    recover form

--- src/app/modules/private/login/login-routing.module.ts
+++ src/app/modules/private/login/login-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
 const routes: Routes = [
   {
     path : '',
     component: LoginComponent
+  },
+  {
+    path : 'recover-password',
+    component: RecoverPasswordComponent
   },
 ];
 
--- src/app/modules/private/login/recover-password/recover-password.component.ts
+++ src/app/modules/private/login/recover-password/recover-password.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recover-password',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recover-password.component.scss']
 })
 export class RecoverPasswordComponent implements OnInit {
+  
+  loading = false;
+  msgErro = '';
 
-  constructor() { }
+  recoverForm: FormGroup = this.fb.group({
+    'email': ['', [Validators.required, Validators.email]],
+  });
+
+  constructor(private fb: FormBuilder,
+    
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.loading = true;
+    const email = this.recoverForm.value.email;
+    
   }
 
 }
