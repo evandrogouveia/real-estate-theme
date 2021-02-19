8ead8b1881091b72bdd96d2247a9ee6ed6e83263
evandrogouveia435 <vando_gouveia@hotmail.com>
2021-02-18 23:24:03 -0300

    recover component

--- src/app/modules/private/login/login.module.ts
+++ src/app/modules/private/login/login.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecoverPasswordComponent } from './recover-password/recover-password.component';
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent, RecoverPasswordComponent],
   imports: [
     CommonModule,
     LoginRoutingModule,
