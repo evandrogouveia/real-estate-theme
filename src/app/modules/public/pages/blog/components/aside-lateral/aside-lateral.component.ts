c73da3dcebce821c8a6737614a7fdd3e2c4e6a6e
evandrogouveia <vando_gouveia@hotmail.com>
2021-03-13 14:38:20 -0300

    categories aside lateral

--- src/app/modules/public/pages/blog/components/aside-lateral/aside-lateral.component.ts
+++ src/app/modules/public/pages/blog/components/aside-lateral/aside-lateral.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from 'src/app/modules/private/admin/components/blog/models/category.model';
+import { BlogService } from '../../services/blog.service';
 
 @Component({
   selector: 'app-aside-lateral',
@@ -6,10 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aside-lateral.component.scss']
 })
 export class AsideLateralComponent implements OnInit {
+  categories$: Observable<Category[]>;
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
   ngOnInit(): void {
+    this.categories$ = this.blogService.getCategory();
   }
 
 }
--- src/app/modules/public/pages/blog/services/blog.service.ts
+++ src/app/modules/public/pages/blog/services/blog.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Category } from 'src/app/modules/private/admin/components/blog/models/category.model';
 import { Post } from 'src/app/modules/private/admin/components/blog/models/post.model';
 
 @Injectable({
@@ -10,11 +11,19 @@ export class BlogService {
   private postsCollection: AngularFirestoreCollection<Post> = this.afs.collection('posts', ref => {
     return ref.orderBy('titlePost', 'asc');
   });
+  private categoriesCollection: AngularFirestoreCollection<Category> = this.afs.collection('categories', ref => {
+    return ref.orderBy('name', 'asc');
+  });
 
   constructor(private afs:AngularFirestore) { }
 
   //LISTAR POSTS
   getPosts(){
     return this.postsCollection.valueChanges();
+  }
+
+  //LISTAR CATEGORIAS
+  getCategory(){
+    return this.categoriesCollection.valueChanges();
   }
 }
