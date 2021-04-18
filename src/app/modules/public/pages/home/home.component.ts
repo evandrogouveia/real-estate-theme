0b34c86ccdae150cf96364d135810adf0e472fb7
evandrogouveia435 <vando_gouveia@hotmail.com>
2021-04-18 18:09:15 -0300

    posts home

--- src/app/modules/public/pages/home/home.component.ts
+++ src/app/modules/public/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
+import { Observable } from 'rxjs';
+import { Post } from 'src/app/modules/private/admin/components/blog/models/post.model';
+import { BlogService } from '../blog/services/blog.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,12 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  posts$: Observable<Post[]>
 
-  constructor() { }
+  constructor(private blogService: BlogService) { }
 
   ngOnInit(): void {
+    this.posts$ = this.blogService.getPosts();
   }
 
   customOptions: OwlOptions = {
--- src/app/modules/public/pages/home/home.module.ts
+++ src/app/modules/public/pages/home/home.module.ts
@@ -1,11 +1,14 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { SharedModule } from '../shared/shared.module';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
 
 
 @NgModule({
@@ -15,6 +18,12 @@ import { SharedModule } from '../shared/shared.module';
     HomeRoutingModule,
     CarouselModule ,
     SharedModule
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt',
+    },
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
