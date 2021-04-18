61b5615d3f249f5ea54ff62d23e3d3e65a32d675
evandrogouveia435 <vando_gouveia@hotmail.com>
2021-04-18 19:01:59 -0300

    recent posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', useHash: true })],
+  imports: [RouterModule.forRoot(routes, 
+    { scrollPositionRestoration: 'enabled', useHash: true }
+    )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/modules/public/pages/blog/components/aside-lateral/aside-lateral.component.ts
+++ src/app/modules/public/pages/blog/components/aside-lateral/aside-lateral.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Category } from 'src/app/modules/private/admin/components/blog/models/category.model';
 import { Post } from 'src/app/modules/private/admin/components/blog/models/post.model';
@@ -13,11 +14,20 @@ export class AsideLateralComponent implements OnInit {
   categories$: Observable<Category[]>;
   posts$: Observable<Post[]>
 
-  constructor(private blogService: BlogService) { }
+  constructor(
+    private blogService: BlogService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.categories$ = this.blogService.getCategory();
     this.posts$ = this.blogService.getPosts();
+  }
+
+  reload(link){
+    this.router.navigateByUrl(`/blog/single-blog/${link}`).then(()=> {
+      location.reload()
+    });
   }
 
 }
