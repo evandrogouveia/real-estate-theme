47d2d3151b44812ee9a284f20a8e9e2ced28a863
vandogouveia <vando_gouveia@hotmail.com>
2021-10-16 13:41:16 -0300

    ajustes

--- src/app/modules/private/admin/components/properties/list-property/list-property.component.ts
+++ src/app/modules/private/admin/components/properties/list-property/list-property.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
+import { Observable } from 'rxjs';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { Property } from '../models/property.model';
+import { PropertyService } from '../services/property.service';
 
 @Component({
   selector: 'app-list-property',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-property.component.scss']
 })
 export class ListPropertyComponent implements OnInit {
+  properties$: Observable<Property[]>
+  dataInput: string;
 
-  constructor() { }
+  constructor(
+    private propertyService: PropertyService,
+    private modalService: BsModalService,
+    public bsModalRef: BsModalRef,
+  ) { }
 
   ngOnInit(): void {
+    this.properties$ = this.propertyService.getProperties();
+  }
+
+  openModalConfirmDelete(p){
+    const initialState = {
+      titleModal: 'Deseja realmente excluir este Imóvel?',
+      titlePost: p.titleProperty,
+      callback: (result) => {//recebe o evento callback true do modal
+        if (result == true){
+          this.delete(p);
+        }
+      }
+    };
+
+    this.bsModalRef = this.modalService.show(
+      ModalComponent,
+      Object.assign({initialState}, {class: 'modal-all'}),
+    );
+
+  }
+
+  delete(p: Property){
+    this.propertyService.deleteProperty(p);
   }
 
 }
