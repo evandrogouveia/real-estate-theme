6d52df3389afe61a78091d4a690a2369ce73aa94
vandogouveia <vando_gouveia@hotmail.com>
2024-07-11 13:58:46 -0300

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: 'https://api-realestate.ceplenario.app.br',
+  //API_URL: 'https://api-realestate.ceplenario.app.br',
+  API_URL: 'http://localhost:3001',
   API_IBGE: 'https://servicodados.ibge.gov.br/api/v1/localidades/estados',
 };
 
